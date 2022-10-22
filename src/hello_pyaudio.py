a370ca0c2cd32e418c261551ee7e168eabcf97be
Georg Becker <georg.becker@shellrider.com>
2022-10-22 13:51:13 +0200

    Creates script that records from microphone and diplays recorded bytes

---
+++ src/hello_pyaudio.py
@@ -0,0 +1,31 @@
+import pyaudio
+
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+RECORD_SECONDS = 0.04
+
+p = pyaudio.PyAudio()
+
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK)
+
+print("* recording")
+
+frames = []
+
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    frames.append(data)
+
+print("* done recording")
+
+stream.stop_stream()
+stream.close()
+p.terminate()
+
+print(frames)
