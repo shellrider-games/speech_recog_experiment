f99cfbfbb09bd101e58b3f0ce35151a6816a4127
Georg Becker <georg.becker@shellrider.com>
2022-10-26 11:05:57 +0200

    Somewhat use FFT

--- src/hello_pyaudio.py
+++ src/hello_pyaudio.py
@@ -1,18 +1,17 @@
+import math
 from tkinter import TclError
 import pyaudio
 import numpy as np
 import matplotlib.pyplot as plt
 
-CHUNK = 1024
+CHUNK = 1024*2
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 44100
 SECONDS = 60
 
-
 def analyseAudio(data):
-    print(data)
-
+    pass
 
 fig, (ax1, ax2) = plt.subplots(1, 2)
 
@@ -24,10 +23,11 @@ ax1.set_ylabel('volume')
 ax1.set_ylim(-2**15, 2**15)
 ax1.set_xlim(0, 2*CHUNK)
 
-ax2.set_title('CURRENT PHONEME')
-phoneme_text = ax2.text(0.5, 0, "Phoneme here")
-ax2.set_ylim(-1, 1)
-ax2.set_xlim(0, 1)
+ax2.set_title('FFT')
+ax2.set_xlabel('Frequency (Hz)')
+ax2.set_ylim(0, 2**16)
+ax2.set_xlim(0,CHUNK)
+line2, = ax2.plot(x, np.random.rand(CHUNK), '-', lw=2)
 
 plt.setp(ax1, xticks=[0, CHUNK, 2 * CHUNK])
 plt.show(block=False)
@@ -40,8 +40,11 @@ input = p.open(RATE, CHANNELS, FORMAT, input=True, frames_per_buffer=CHUNK)
 while(True):
     data = input.read(CHUNK)
     numericData = np.frombuffer(data, dtype=np.int16)
-    # analyseAudio(numericData)
+
+    analyseAudio(numericData)
+
     line.set_ydata(numericData)
+    line2.set_ydata(np.abs(np.fft.fft(numericData)))
     try:
         fig.canvas.draw()
         fig.canvas.flush_events()
